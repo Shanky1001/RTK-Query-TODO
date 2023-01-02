30e1e9470bd92b6355184be381f46f916c78e7dc
Shashank Rai <108118506+shanky-ced@users.noreply.github.com>
2023-01-02 09:38:51 +0530

    Update Types.tsx

--- src/typescript/types/Types.tsx
+++ src/typescript/types/Types.tsx
@@ -3,4 +3,4 @@ import { todos } from "../interfaces/Interfaces";
 
 
 export type todoAction = { type: "addToDo", payload: todos } | { type: "deleteToDo", payload: { id: number } } | { type: 'completed', payload: { id: number } } | { type: 'incomplete', payload: { id: number } };
-export type StoreType = ReturnType<typeof Store.getState>
+export type StoreType = ReturnType<typeof Store.getState>;
